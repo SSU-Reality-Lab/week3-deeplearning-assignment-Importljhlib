9fcce169c5ab486b3b45d31465338c4c32041328
Importljhlib <gogozzi1102@gmail.com>
2025-12-20 20:11:27 +0900

    20241848이재현

--- utils/rnn_layers.py
+++ utils/rnn_layers.py
@@ -34,8 +34,8 @@ def rnn_step_forward(x, prev_h, Wx, Wh, b):
     # hidden state and any values you need for the backward pass in the next_h   #
     # and cache variables respectively.                                          #
     ##############################################################################
-    next_h = ________________________________________
-    cache = _________________________________________
+    next_h = np.tanh(x.dot(Wx) + prev_h.dot(Wh) + b)
+    cache = (x, prev_h, Wx, Wh, next_h)
     ##############################################################################
     #                               END OF YOUR CODE                             #
     ##############################################################################
@@ -64,14 +64,15 @@ def rnn_step_backward(dnext_h, cache):
     # HINT: For the tanh function, you can compute the local derivative in terms #
     # of the output value from tanh.                                             #
     ##############################################################################
-    x, prev_h, Wx, Wh, b, tanh = cache
+    x, prev_h, Wx, Wh, tanh = cache
     
-    dtanh = ___________                               #[NxH]
-    dnext_tanh = _______________                      #[NxH]
-    dx = ____________________                         #[NxD]
-    dprev_h = ____________________                    #[NxH]
-    dWx = _____________________                       #[DxH]
-    dWh = __________________________                  #[DxH]
+    dtanh = 1 - tanh**2                            #[NxH]
+    dnext_tanh = dnext_h * dtanh                      #[NxH]
+    dx = dnext_tanh.dot(Wx.T)                         #[NxD]
+    dprev_h = dnext_tanh.dot(Wh.T)                    #[NxH]
+    dWx = x.T.dot(dnext_tanh)                       #[DxH]
+    dWh = prev_h.T.dot(dnext_tanh)                 #[DxH]
+    db = dnext_tanh.sum(axis=0)
     ##############################################################################
     #                               END OF YOUR CODE                             #
     ##############################################################################
@@ -108,7 +109,7 @@ def rnn_forward(x, h0, Wx, Wh, b):
     h = np.zeros((N, T, H))
     cache = []
     for i in range(T):
-        prev_h, cache_h = rnn_step_forward(_______, _______, _______, _______, _______)
+        prev_h, cache_h = rnn_step_forward(x[:, i, :], prev_h, Wx, Wh, b)
         h[:,i,:] = prev_h
         cache.append(cache_h)
     ##############################################################################
@@ -143,12 +144,12 @@ def rnn_backward(dh, cache):
     # defined above. You can use a for loop to help compute the backward pass.   #
     ##############################################################################
     N, T, H = dh.shape
-    dxl, dprev_h, dWx, dWh, db = rnn_step_backward(________, ________)
+    dxl, dprev_h, dWx, dWh, db = rnn_step_backward(dh[:, T-1, :],  cache[T-1])
     D = dxl.shape[1]
     dx = np.zeros((N,T,D))
     dx[:,T-1,:] = dxl
     for i in range(T-2, -1, -1):
-        dxc, dprev_hc, dWxc, dWhc, dbc = rnn_step_backward(________+________, ________)
+        dxc, dprev_hc, dWxc, dWhc, dbc = rnn_step_backward(dh[:, i, :] + dprev_h, cache[i])
         dx[:,i,:] += dxc
         dprev_h = dprev_hc
         dWx += dWxc
@@ -182,8 +183,8 @@ def word_embedding_forward(x, W):
     #                                                                            #
     # HINT: This can be done in one line using NumPy's array indexing.           #
     ##############################################################################
-    out = _____
-    cache = _____
+    out = W[x]
+    cache = (x, W)
     ##############################################################################
     #                               END OF YOUR CODE                             #
     ##############################################################################
